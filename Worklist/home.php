4ce545c6538701d494593604d8954373112bafc3
DWISH2 <DWISH2@users.noreply.github.com>
2017-11-07 22:22:25 +0100

    Home.php
    home.php is de nieuwe landingpage van Worklist

---
+++ Worklist/home.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Worklist</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/shapecss@2.1.3/public/css/shape.css">
+    <script src="https://cdn.jsdelivr.net/npm/shapecss@2.1.3/public/js/shape.js" async></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<nav class="">
+	<!--for logo-->
+	<div class="logo">
+ 		<a href="#">
+   			<img src="worklist-logo.png" width="240px">
+   			<span></span>
+ 		</a>
+	</div>
+	<!--for responsive-->
+	<button class="for-mobile bg-primary"></button>
+
+	<!--nav items-->
+	<ul class="item">
+ 		
+	</ul>
+ 	<ul class="item right">
+	 	<li><a class="bu1" href="login.php">Login</a></li>
+	 	<li><a class="bu1" href="#">About</a></li>
+ 	</ul>
+	</nav>
+
+	<div class="row">
+		<img class="img-responsive" src="img/header1-01.jpg" style="width: 100%; height: 100%;">
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-3">
+				<div class="card">
+					<div class="body">
+						<h3 class="primary">Welcome!</h3>
+						<h6 class="info">Worklist is a students initiative </h6>
+					</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<div class="card">
+					<div class="body">
+						<h3 class="primary">Welcome!</h3>
+						<h6 class="info">Worklist is a students initiative </h6>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-3">
+				<img class="img-responsive" src="img/project.svg">	
+				<div class="card">
+					<div class="header primary">
+						<h3 class="text-center">Project backlog</h3>
+        				
+    				</div>
+    				<div class="body">
+        				<p>Before a project starts, it's important all tasks are determined.</p>
+        				<p>Worklist offers a project overview with a clear view on the tasks.</p>
+    				</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<img class="img-responsive" src="img/task.svg">
+				<div class="card">
+					<div class="header primary">
+						<h3 class="text-center">Sprint backlog</h3>
+        				
+    				</div>
+    				<div class="body">
+        				<p>Once all tasks are determined, it's time to prioritize</p>
+        				<p>Through color coding and due dates, worklist gives a good idea on what has to be done.</p>
+    				</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<img class="img-responsive" src="img/sprinting.svg">
+				<div class="card">
+					<div class="header primary">
+						<h3 class="text-center">The sprint</h3>
+        				
+    				</div>
+    				<div class="body">
+        				<p>Sprinting is the actual production of a task in a 2-4 weeks sprint.</p>
+        				<p>Communication is a must in this step, we suggest meetings every 24h.</p>
+    				</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<img class="img-responsive" src="img/produce.svg">
+				<div class="card">
+					<div class="header primary">
+						<h3 class="text-center">Product increment</h3>
+        				
+    				</div>
+    				<div class="body">
+        				<p>All these steps eventually lead to a complete product, every task creates a new function.</p>
+        				<p>This on turn leads to a complete project or product ready for use.</p>
+    				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+	<footer class="bg-dark copyright">
+		<span>Worklist</span>
+		<span>© All Right Reserved 2017</span>
+		<span>A Splinter® product</a></span>
+	</footer>
+
+</body>
+</html>
